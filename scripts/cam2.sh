0224f12e27640fcc396507da671cf94962755f73
sebastian <sebastian@lsebastians-ste.de>
2014-05-29 09:27:56 +0200

    Added scripts for cameras

---
+++ scripts/cam2.sh
@@ -0,0 +1,2 @@
+ #!/bin/bash
+ ./cam2feed.sh /tmp/feed2  "http://gstreamer:GlytEnru@192.168.50.101/videostream.cgi?rate=0"
--- scripts/camtest.sh
+++ scripts/camtest.sh
@@ -1,4 +1,4 @@
-MIXERCAPS='video/x-raw-rgb, bpp=(int)32, depth=32, endianness=(int)4321, red_mask=(int)65280, green_mask=(int)16711680, blue_mask=(int)-16777216, width=(int)1024, height=(int)768, framerate=(fraction)24/1, pixel-aspect-ratio=(fraction)1/1, interlaced=(boolean)false'
+MIXERCAPS='video/x-raw-rgb, width=(int)1024, height=(int)768'
 
 
-gst-launch-0.10 -v souphttpsrc location=http://schumb:frederik@192.168.11.10/videostream.cgi?rate=0 ! jpegdec  ! ffmpegcolorspace ! videoscale ! ffmpegcolorspace ! $MIXERCAPS ! ximagesink
+gst-launch-0.10 -v souphttpsrc location=http://gstreamer:GlytEnru@$1/videostream.cgi?rate=0 ! jpegdec  ! ffmpegcolorspace ! videoscale ! ffmpegcolorspace ! $MIXERCAPS ! ximagesink
