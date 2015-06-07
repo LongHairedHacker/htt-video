7269c2a6786b20dce3bda66ccd0e5ceb3063a629
LongHairedHacker <sebastian@sebastians-site.de>
2015-06-07 19:13:30 +0200

    Stuff seems to work

---
+++ scripts/test720pfeed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+MIXERFORMAT='video/x-raw-rgb, bpp=(int)32, endianness=(int)4321, format=(fourcc)BGRA, red_mask=(int)65280, green_mask=(int)16711680, blue_mask=(int)-16777216, width=(int)1280, height=(int)720, framerate=(fraction)24/1, pixel-aspect-ratio=(fraction)1/1, interlaced=(boolean)false'
+SRC="filesrc location=../videos/tears_of_steel_720p.mkv ! matroskademux ! h264parse ! ffdec_h264"
+SCALE='ffmpegcolorspace ! videoscale ! ffmpegcolorspace'
+gst-launch-0.10 -v    \
+	$SRC            !\
+	$SCALE 			!\
+	$MIXERFORMAT	!\
+    shmsink socket-path=/tmp/feed1 shm-size=10000000 wait-for-connection=0
+
